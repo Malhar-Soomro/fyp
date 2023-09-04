251c9ae1c6ff03e07a59f959b4c0a8423b925357
Malhar-Soomro <malharsoomro2@gmail.com>
2023-09-05 01:34:17 +0500

    redirecting unauthorized users to login page

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import Sidebar from '../../components/Sidebar';
+import Swal from 'sweetalert2'
+import { navigate } from 'gatsby';
+
 
 import * as styles from "../../styles/dashboard.module.css"
 
 
 const Dashboard = () => {
+
+    const user = sessionStorage.getItem("uid");
+    if (!user) {
+        Swal.fire({
+            icon: "error",
+            title: "create your account",
+            showConfirmButton: false,
+            timer: 2000
+        });
+        return navigate("/login")
+    }
     return (
         <div className={styles.container}>
             <Sidebar />
--- src/pages/dashboard/profile.js
+++ src/pages/dashboard/profile.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import Sidebar from '../../components/Sidebar'
+import Swal from 'sweetalert2'
+import { navigate } from 'gatsby';
+
 
 import * as styles from "../../styles/profile.module.css"
 
 const Profile = () => {
+
+    const user = sessionStorage.getItem("uid");
+    if (!user) {
+        Swal.fire({
+            icon: "error",
+            title: "create your account",
+            showConfirmButton: false,
+            timer: 2000
+        });
+        return navigate("/login")
+    }
+
     return (
         <div className={styles.container}>
             <Sidebar />
