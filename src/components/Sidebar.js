91a2d22169339edb8827e8c5780f7c4c9c7dba09
Malhar-Soomro <malharsoomro2@gmail.com>
2023-09-05 01:35:21 +0500

    added bg color to selected sidebar item

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import { Link } from 'gatsby'
+import { Link, navigate } from 'gatsby'
 import React from 'react'
 import { UilCreateDashboard, UilMessage, UilUserCircle } from '@iconscout/react-unicons'
 import * as styles from "../styles/sidebar.module.css";
@@ -6,15 +6,17 @@ import * as styles from "../styles/sidebar.module.css";
 const Sidebar = () => {
     return (
         <div className={styles.container}>
-            <Link to='/dashboard'>
+            <button className={styles.arrowButton} onClick={() => navigate("/")} >←</button>
+
+            <Link to='/dashboard' activeStyle={{ backgroundColor: "#152953" }}>
                 <UilCreateDashboard />
                 <span>Dashboard</span>
             </Link>
-            <Link to='/dashboard/requests'>
+            <Link to='/dashboard/requests' activeStyle={{ backgroundColor: "#152953" }}>
                 <UilMessage />
                 <span>Requests</span>
             </Link>
-            <Link to='/dashboard/profile'>
+            <Link to='/dashboard/profile' activeStyle={{ backgroundColor: "#152953" }}>
                 <UilUserCircle />
                 <span>Profile</span>
             </Link>
